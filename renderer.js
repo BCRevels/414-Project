b8181292928086cef0f300ecd8dc8701badcee1a
Taylor <stomonain@gmail.com>
2019-10-18 13:53:24 -0500

    Added URL functionality
    Added a text box on the main download page.  Type a URL in the box and hit the download button to download a video.  Should work as long as the element id and the input remains as text of some kind.  Feel free to mess with the CSS or different input types to make it look nice/fit in.

--- renderer.js
+++ renderer.js
@@ -3,15 +3,14 @@
 // All of the Node.js APIs are available in this process.
 const { exec } = require('child_process');
 
-function hiThere() {
-	exec('youtube-dl --no-playlist https://www.youtube.com/watch?v=QZeto4CIf84', (error, stdout, stderr) => {
+function youtubeDlDownload() {
+	//alert("youtube-dl --no playlist " + document.getElementById('videoInput').value);
+	exec('youtube-dl --no-playlist ' + document.getElementById('videoInput').value, (error, stdout, stderr) => {
 	if (error) {
 		alert(`exec error: ${error}`);
 		return;
 	}
 	alert(`stdout: ${stdout}`);
 	alert(`stderr: ${stderr}`);
-});
+	});
 }
-
-document.querySelector('#btnEd').addEventListener('click', () => {hiThere() })

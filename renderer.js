b13e3a1d143c6f0a409da01e5644ac4e8d5842db
Taylor <stomonain@gmail.com>
2019-11-16 11:30:17 -0600

    Added Download Directory Selection and Percentage Tracker
    User can now select download directory for their videos, all of which are currently stored as mp4.  There is also now a percentage tracker in Index and a spinner gif for when a video begins downloading.

--- renderer.js
+++ renderer.js
@@ -1,10 +1,10 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-const { exec } = require('child_process');
 var { spawn } = require('child_process');
-var child = require('child_process');
 const Storage = require('./storage.js');
+const electron = require('electron').remote;
+var child = require('child_process');
 
 //this is the data file for long-term storage
 const storage = new Storage({
@@ -27,47 +27,62 @@ if(histContent.length > 1) {
 alert(histContent);
 */
 
+async function openDialog() {
+	var filePath = await electron.dialog.showOpenDialog({
+		properties:['openDirectory'],
+		filers:[
+			{name:'Log', extensions:['csv', 'log']}
+		]
+	});
+	electron.app.setPath('videos', filePath.filePaths[0]);
+	document.getElementById('downloadDirectory').value = filePath.filePaths[0];
+}
+
 function youtubeDlDownload() {
-	var url = document.getElementById('videoInput').value;
-	var temp = child.spawn('cmd.exe', ['/c', 'runYoutubeDl.bat \"' + url + '\"'], {shell: true});
+	//make spinner and loading percentage visible
+	document.getElementById('spinner').style.visibility = 'visible';
+	document.getElementById('loadingPercent').innerHTML = '0%';
 	
-	//this info is good for the loading bar
+	//get input and build values for sending to 
+	var dlRegex = new RegExp('[\\\/\=\:\.]', 'g');
+	var url = document.getElementById('videoInput').value;
+	dlPath = electron.app.getPath('videos') + '\\\%(title)s.mp4';
+	//youtube-dl -o 'C:\Users\User\Downloads\%(title)s.%(ext)s' www.youtube.com/video
+	
+	//start video download
+	var temp = child.spawn('cmd.exe', ['/c', 'runYoutubeDl.bat \"' + dlPath + '\" \"' + url + '\"'], {shell: true});
+	
+	//////child events//////
+	
+	//this info is good for the loading bar and percent tracker
 	temp.stdout.on('data', (data) => {
+		console.log("DATA: " + data);
 	    var regex = new RegExp(']\\s+[0-9]+\.?[0-9]+');	//regular expression for getting percentage done
 		var dlpercent = data.toString().match(regex);			//get percentage done
+		if(dlpercent) {
 		dlpercent = dlpercent[dlpercent.length-1];				
 		dlpercent = dlpercent.slice(2, dlpercent.length);		
-		dlpercent = parseFloat(dlpercent);						
-		let loadingBar = document.getElementById('loadingBar'); //change from string to float
-		loadingBar.style.width = dlpercent + '%';						//update loading bar
+		dlpercent = parseFloat(dlpercent);							//change from string to float
+		document.getElementById('loadingBar').style.width = dlpercent + '%';				//update loading bar
+		document.getElementById('loadingPercent').innerHTML = dlpercent + '%';
+		}
 	});
 	
 	temp.stderr.on('data', (data) => {
-		alert(data.toString());
-		let loadingBar = document.getElementById('loadingBar');
-		loadingBar.style.width = 0;
-	  
+		console.log(data.toString());
+		document.getElementById('loadingBar').style.width = 0;
+		document.getElementById('loadingPercent').value = "";
 	});
 
 	temp.on('exit', (code) => {
-		let loadingBar = document.getElementById('loadingBar');
-		loadingBar.style.width = 0;
-		alert("Finished Downloading");
-	});
-	
-	/*
-	child.exec('youtube-dl --no-playlist ' + url, (error, stdout, stderr) => {
-		if (error) {
-			alert(`exec error: ${error}`);
-			return;
-		}
-		//save to history in long-term storage
-		let his = storage.get('history');				//get history array from storage file
-		his.push(url);								//push the url on the history array
-		storage.set('history', his);
+		document.getElementById('loadingBar').style.width = 0;
+		document.getElementById('loadingPercent').innerHTML = "";
 		
-		alert(`stdout: ${stdout}`);
-		alert(`stderr: ${stderr}`);
+		//add to history
+		let his = storage.get('history');                //get history array from storage file
+		his.push(url);                                	 //push the url on the history array
+		storage.set('history', his);
+		console.log("Finished Downloading");
+		document.getElementById('spinner').style.visibility = 'hidden';
 	});
-	*/
 }
